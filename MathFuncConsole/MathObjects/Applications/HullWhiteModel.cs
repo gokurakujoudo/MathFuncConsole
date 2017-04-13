b6f2bd19a9f4b03621810fb3c99da912aabaf409
gokurakujoudo <louyuming23@gmail.com>
2017-04-13 00:30:31 -0500

    change: fix bugs and enhance performance in HW model and MC

--- MathFuncConsole/MathObjects/Applications/HullWhiteModel.cs
+++ MathFuncConsole/MathObjects/Applications/HullWhiteModel.cs
@@ -19,25 +19,26 @@ namespace MathFuncConsole.MathObjects.Applications {
             this.Sigma = Input(sigma);
         }
 
-        public (double at0t1, double bt0t1) HW_AB(double t0, double t1) {
+        private (double at0t1, double bt0t1) HW_AB(double t0, double t1) {
             // 3.39
             var pmT0 = this.MarketP(t0);
             var pmT1 = this.MarketP(t1);
             var fmT0 = this.MarketF(t0);
-            var bT0T1 = (1 - Math.Exp(-this.A() * (t1 - t0))) / this.A();
+            var a = this.A();
+            var bT0T1 = (1 - Math.Exp(-a * (t1 - t0))) / a;
             var aT0T1 = pmT1 / pmT0 * Math.Exp(bT0T1 * fmT0 -
-                                               Math.Pow(this.Sigma(), 2) / (4 * this.A()) *
-                                               (1 - Math.Exp(-2 * this.A() * t0)) * Math.Pow(bT0T1, 2));
+                                               Math.Pow(this.Sigma(), 2) / (4 * a) * (1 - Math.Exp(-2 * a * t0)) *
+                                               Math.Pow(bT0T1, 2));
             return (aT0T1, bT0T1);
         }
 
-        public double HW_ZBPrice_CF(double t0, double t1, double rT0) {
+        private double HW_ZBPrice_CF(double t0, double t1, double rT0) {
             // 3.39
             var(aT0T1, bT0T1) = this.HW_AB(t0, t1);
             return aT0T1 * Math.Exp(-bT0T1 * rT0);
         }
 
-        public double HW_ZBPut_CF(double t0, double t1, double t2, double x, double rT0) {
+        private double HW_ZBPut_CF(double t0, double t1, double t2, double x, double rT0) {
             // 3.41
             var(_, bts) = this.HW_AB(t0, t2);
             var pT0T1 = this.HW_ZBPrice_CF(t0, t1, rT0);
@@ -49,7 +50,7 @@ namespace MathFuncConsole.MathObjects.Applications {
             return price;
         }
 
-        public double HW_Caplets(double t0, double t1, double t2, double x, double n, double rT0) {
+        private double HW_Caplets(double t0, double t1, double t2, double x, double n, double rT0) {
             // 2.27
             var nPrime = n * (1 + x * (t2 - t1));
             var xPrime = 1 / (1 + x * (t2 - t1));
@@ -57,12 +58,12 @@ namespace MathFuncConsole.MathObjects.Applications {
             return cpl;
         }
 
-        public double HW_Caps(double t0, double tAlpha, double[] taoArray, double x, double n, double rT0) {
+        private double HW_Caps(double t0, double tAlpha, double[] taoArray, double x, double n, double rT0) {
             // 3.42
             var term = taoArray.Length;
             var caps = 0D;
             var t1 = tAlpha;
-            var t2 = tAlpha + taoArray[0];
+            var t2 = tAlpha;
             for (var i = 0; i < term; i++) {
                 if (i > 0)
                     t1 = t2;
@@ -72,18 +73,21 @@ namespace MathFuncConsole.MathObjects.Applications {
             return caps;
         }
 
-        public double[] HW_R_CM(double s, double rs, double t0, int n) {
+        private double[] HW_R_CM(double s, double rs, double t0, int n) {
             // 3.37
-            if (t0 == s) return new[] {s};
-            Func<double, double> alphaFunc = (t) => this.MarketF(t) +
-                                                    Math.Pow(this.Sigma(), 2) / (2 * Math.Pow(this.A(), 2)) *
-                                                    Math.Pow(1 - Math.Exp(-this.A() * t), 2);
-            var mu = rs * Math.Exp(-this.A() * (t0 - s)) + alphaFunc(t0) -
-                     alphaFunc(s) * Math.Exp(-this.A() * (t0 - s));
-            var sigma = Math.Pow(this.Sigma(), 2) / (2 * Math.Pow(this.A(), 2)) *
-                        Math.Pow(1 - Math.Exp(-2 * this.A() * (t0 - s)), 2);
+            if (t0 == s) return new[] {rs};
+            var sigma = this.Sigma();
+            var a = this.A();
+
+            double AlphaFunc(double t) => this.MarketF(t) +
+                                          Math.Pow(sigma, 2) / (2 * Math.Pow(a, 2)) * Math.Pow(1 - Math.Exp(-a * t), 2);
+
+            var mu = rs * Math.Exp(-a * (t0 - s)) + AlphaFunc(t0) -
+                     AlphaFunc(s) * Math.Exp(-a * (t0 - s));
+            var stdvar = Math.Pow(sigma, 2) / (2 * Math.Pow(a, 2)) *
+                         Math.Pow(1 - Math.Exp(-2 * a * (t0 - s)), 2);
             var sampleFunc = MonteCarloSimulation.GenerateSamples(
-                n, 0.Wrap(), (t, x) => x, mu.Wrap(), sigma.Wrap());
+                n, 0.Wrap(), (t, x) => x, mu.Wrap(), stdvar.Wrap());
             var samples = sampleFunc();
             return samples;
         }
@@ -103,31 +107,43 @@ namespace MathFuncConsole.MathObjects.Applications {
             return samples.Average();
         }
 
-        private double MarketF(double t) =>
-            Interpolation.Linear(t, _marketT, _marketF);
+
+        private readonly Dictionary<double, double> _mf = new Dictionary<double, double>();
+        private readonly Dictionary<double, double> _mp = new Dictionary<double, double>();
+
+        private double MarketF(double t) {
+            if (_mf.ContainsKey(t))
+                return _mf[t];
+            var marketF = Interpolation.Linear(t, _marketT, _marketF);
+            _mf[t] = marketF;
+            return marketF;
+        }
 
 
-        private double MarketP(double t) =>
-            Interpolation.Linear(t, _marketT, _marketP);
+        private double MarketP(double t) {
+            if (_mp.ContainsKey(t))
+                return _mp[t];
+            var marketP = Interpolation.Linear(t, _marketT, _marketP);
+            _mp[t] = marketP;
+            return marketP;
+        }
 
 
         public static SimulatedAnnealing<HullWhiteModel> HW_SaaSolver(
-            double[] ts, double[] ps, double[] fs, double t0, double tAlpha,
+            double[] marketT, double[] marketP, double[] marketF, double t0, double tAlpha,
             IReadOnlyCollection<(double, double[])> strikeAndTaos, double[] capsTargets, double notional,
             IReadOnlyCollection<(double, double)> ranges, int samplesCount = 100, int threads = 6,
-            int temperature = 400, int iters = 20000, int cooliter = 40, bool debug = false)
-        {
+            int temperature = 400, int iters = 20000, int cooliter = 40, bool debug = false) {
             var m = strikeAndTaos.Count;
-            var paras = new object[] { string.Empty, 1, .1, ts, ps, fs };
+            var paras = new object[] {string.Empty, 1, .1, marketT, marketP, marketF};
             Func<HullWhiteModel, double> obj = (hwx) => {
                 var capFuncs = strikeAndTaos
                     .Select<(double, double[]), Func<double[], double>>(
                         sat => (rts) => rts.Average(r => hwx.HW_Caps(t0, tAlpha, sat.Item2, sat.Item1, notional, r)))
                     .ToArray();
                 var totalDiff = 0D;
-                var rs = hwx.HW_R_CM(0, fs[0], t0, samplesCount);
-                for (var i = 0; i < m; i++)
-                {
+                var rs = hwx.HW_R_CM(0, marketF[0], t0, samplesCount);
+                for (var i = 0; i < m; i++) {
                     var cp = capFuncs[i](rs);
                     var diff = (cp - capsTargets[i]) / (capsTargets[i]);
                     totalDiff += Math.Pow(diff, 2);
@@ -136,7 +152,7 @@ namespace MathFuncConsole.MathObjects.Applications {
             };
             var saaSolver =
                 new SimulatedAnnealing<HullWhiteModel>(
-                    paras, new[] { "A", "Sigma" }, ranges, obj, threads, temperature, iters, cooliter, debug);
+                    paras, new[] {"A", "Sigma"}, ranges, obj, threads, temperature, iters, cooliter, debug);
             return saaSolver;
         }
 
--- MathFuncConsole/Program.cs
+++ MathFuncConsole/Program.cs
@@ -21,27 +21,25 @@ namespace MathFuncConsole {
         }
 
         private static void Demo_HW_SaaSolver() {
-            var ts = new[] {
+            var marketT = new[] {
                 0, 0.255555556, 0.511111111, 0.761111111, 1.013888889, 1.269444444, 1.530555556, 1.775, 2.027777778,
                 2.291666667, 2.541666667, 2.797222222, 3.047222222, 3.302777778, 3.555555556, 3.805555556, 4.058333333,
                 4.313888889, 4.569444444, 4.819444444
             };
 
-            var ps = new[] {
+            var marketP = new[] {
                 1, 0.993664, 0.989978, 0.98618, 0.982123, 0.977784, 0.973149, 0.968688, 0.963922, 0.958764, 0.95371,
                 0.948381, 0.943253, 0.937892, 0.932477, 0.927016, 0.921614, 0.916074, 0.91046, 0.904898
             };
 
-            var fs = new[] {
+            var marketF = new[] {
                 .0115761, 0.0134633, 0.0145677, 0.015404, 0.016344, 0.0173619, 0.0182408, 0.0188542, 0.0195574,
                 0.0203891,
                 0.0212078, 0.0219844, 0.0217523, 0.0223657, 0.0229775, 0.0235635, 0.0231886, 0.0236636, 0.0241312,
                 0.0245827
             };
 
-            var hw = new HullWhiteModel("hw1", .5, .05, ts, ps, fs);
-
-            var t0 = 0;
+            var t0 = 0.2;
             var tAlpha = .5;
             var notional = 1E6;
             var samplesCount = 100;
@@ -51,17 +49,19 @@ namespace MathFuncConsole {
                 (.03, new[] {.25, .25, .25, .25, .25, .25, .25, .25}),
                 (.04, new[] {.25, .25, .25, .25, .25, .25, .25, .25, .25, .25}),
             };
+
+            var hwTarget = new HullWhiteModel("hw1", .5, .05, marketT, marketP, marketF);
             var capsTargets = strikeAndTaos
-                .Select(sat => hw.HW_CAP_CM(t0, tAlpha, sat.Item2, sat.Item1, notional, samplesCount))
+                .Select(sat => hwTarget.HW_CAP_CM(t0, tAlpha, sat.Item2, sat.Item1, notional, samplesCount))
                 .ToArray();
 
             var temperature = 500;
             var iters = 20000;
             var cooliter = 40;
-            var ranges = new[] {(0D, 5), (0D, 0.5)};
+            var ranges = new[] {(0D, 5D), (0D, 0.5D)};
             var threads = 5;
 
-            var saaSolver = HullWhiteModel.HW_SaaSolver(ts, ps, fs, t0, tAlpha, strikeAndTaos, capsTargets, notional,
+            var saaSolver = HullWhiteModel.HW_SaaSolver(marketT, marketP, marketF, t0, tAlpha, strikeAndTaos, capsTargets, notional,
                                                         ranges, samplesCount, threads, temperature, iters, cooliter,
                                                         debug: true);
 
